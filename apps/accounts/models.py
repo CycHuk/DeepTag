a60cc8c4b0f7cb13cc85910e31ac23a144bfb9d8
Dmitry Langer <DimaLanger@outlook.com>
2026-03-06 12:36:02 +0700

    feat: create User model

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,45 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email обязателен')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if not extra_fields.get('first_name'):
+            raise ValueError('Суперпользователь должен иметь first_name')
+        if not extra_fields.get('last_name'):
+            raise ValueError('Суперпользователь должен иметь last_name')
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True)
+
+    first_name = models.CharField(max_length=30, blank=False)
+    last_name = models.CharField(max_length=30, blank=False)
+    middle_name = models.CharField(max_length=30, blank=True, null=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    object = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'middle_name']
+
+    class Meta:
+        db_table = 'users'
