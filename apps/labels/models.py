c6544963edbe4e35f02a04d2f4618260f663e27e
Dmitry Langer <DimaLanger@outlook.com>
2026-03-06 17:14:58 +0700

    feat: add labels models

--- apps/labels/models.py
+++ apps/labels/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+from apps.projects.models import Project
+
+
+class Label(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    project = models.ForeignKey(Project, related_name="labels", on_delete=models.CASCADE)
+
+    name = models.CharField(max_length=30, blank=False, null=False)
+    color = models.CharField(max_length=7, blank=False,null=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "labels"
+        unique_together = ("project", "name")
+
+
