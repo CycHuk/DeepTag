16af6d6dff2a88413a0439d4440753c5bac82d77
Dmitry Langer <DimaLanger@outlook.com>
2026-03-06 12:48:33 +0700

    feat: add admin support for User

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
+from .models import User
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'middle_name', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active', 'is_superuser', 'groups')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Личная информация', {'fields': ('first_name', 'last_name', 'middle_name')}),
+        ('Права и группы', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Важные даты', {'fields': ('last_login',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email', 'first_name', 'last_name', 'middle_name', 'password1', 'password2', 'is_staff', 'is_active')}
+         ),
+    )
+
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+    filter_horizontal = ('groups', 'user_permissions',)
+
+
+admin.site.register(User, UserAdmin)
+
+admin.site.unregister(Group)
