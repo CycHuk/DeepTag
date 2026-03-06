89d4e893d4234e1438911ab4e43048d9b034ef18
Dmitry Langer <DimaLanger@outlook.com>
2026-03-06 16:28:58 +0700

    refactor: rename Projects table

--- apps/projects/models.py
+++ apps/projects/models.py
@@ -10,3 +10,6 @@ class Project(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "projects"
