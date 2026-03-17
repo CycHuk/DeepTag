463bdc0d9aeab4637229065fbe3c04c38bcfe5f2
Dmitry Langer <DimaLanger@outlook.com>
2026-03-17 10:19:20 +0700

    feat: add tasks section on project page

--- apps/projects/views.py
+++ apps/projects/views.py
@@ -1,16 +1,20 @@
 from django.contrib.auth.mixins import UserPassesTestMixin
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView, DetailView, DeleteView, UpdateView
 
 from .forms import ProjectCreateForm, LabelsFormSet
 from .models import Project
+from ..tasks.models import Task
+
 
 class ProjectListView(ListView):
     model = Project
     template_name = "projects/index.html"
     paginate_by = 6
     context_object_name = "projects"
+
 
 class ProjectCreateView(CreateView):
     template_name = "projects/create.html"
@@ -21,7 +25,6 @@ class ProjectCreateView(CreateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         form_data = self.request.session.pop('form_projects_create', None)
-
 
         if form_data:
             context['form'] = self.form_class(form_data)
@@ -56,12 +59,57 @@ class ProjectCreateView(CreateView):
 def add_label_form(request):
     form_index = request.GET.get("labels_formset-TOTAL_FORMS", 0)
 
-    return render(request,"projects/label_form_row.html", {'index': form_index})
+    return render(request, "projects/label_form_row.html", {'index': form_index})
+
 
 class ProjectDetailView(DetailView):
     model = Project
-    template_name = "projects/detail.html"
     context_object_name = "project"
+    paginate_by = 8
+
+    def get_template_names(self):
+        if self.request.headers.get("HX-Request"):
+            return ["tasks/task_cards.html"]
+        return ["projects/detail.html"]
+
+    def get_filtered_tasks(self):
+        tasks = self.object.tasks.all()
+
+        filter_type = self.request.GET.get("filter")
+        task_type = self.request.GET.get("type")
+        sort_order = self.request.GET.get("sort", "desc")
+
+        if filter_type == "my" and self.request.user.is_authenticated:
+            tasks = tasks.filter(assigned_to=self.request.user)
+
+        elif filter_type == "free":
+            tasks = tasks.filter(assigned_to__isnull=True)
+
+        elif filter_type == "completed":
+            tasks = tasks.filter(status=Task.Status.COMPLETED)
+
+        if task_type:
+            tasks = tasks.filter(type=task_type)
+
+        if sort_order == "asc":
+            tasks = tasks.order_by("created_at")
+        else:
+            tasks = tasks.order_by("-created_at")
+
+        return tasks
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        tasks = self.get_filtered_tasks()
+
+        paginator = Paginator(tasks, self.paginate_by)
+        page = self.request.GET.get("page")
+
+        context["page_tasks"] = paginator.get_page(page)
+
+        return context
+
 
 class ProjectDeleteView(UserPassesTestMixin, DeleteView):
     model = Project
@@ -69,6 +117,7 @@ class ProjectDeleteView(UserPassesTestMixin, DeleteView):
 
     def test_func(self):
         return self.request.user.is_superuser
+
 
 class ProjectEditView(UpdateView):
     model = Project
